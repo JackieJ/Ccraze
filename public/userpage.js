064f0f8b6bd87951d48ccbcab4042f4cbb4b149f
Jackie Jin <liveaeon@gmail.com>
2013-10-19 05:09:36 -0700

    userpagejs

--- public/userpage.js
+++ public/userpage.js
@@ -1,5 +1,27 @@
 //userpage.js
 
+function JawBone(url) {
+    this.url = url;
+    this.client_id = "ITn61XJrBlU";
+    this.appSecret = "4e6d0aaf44a60ac372938d44dc51ed7f91f7fea6";
+        
+    var getPToken = function () {
+        var matchResult = url.match(/\?code=(.*)/);
+        var tempToken = "";
+        var pTokenPrefix = "https://jawbone.com/auth/oauth2/token?client_id=ITn61XJrBlU&client_secret=4e6d0aaf44a60ac372938d44dc51ed7f91f7fea6&grant_type=authorization_code&code=";
+        var pTokenURL = pTokenPrefix;
+        if(matchResult) {
+            tempToken = matchResult[1];
+            pTokenURL += tempToken;
+            console.log(pTokenURL);
+        }
+    }
+
+}
+
 $(document).ready(function() {
-    console.log(window.location.href);
+    //console.log(window.location.href);
+    console.log("hello!");
+    var jb = new JawBone(window.location.href);
+    jb.getPToken();
 }
