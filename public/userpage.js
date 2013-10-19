6ae83d4d704f93e8e2bde7148f28dbc62c791fa1
Jackie Jin <liveaeon@gmail.com>
2013-10-19 05:43:24 -0700

    user.jade

--- public/userpage.js
+++ public/userpage.js
@@ -2,18 +2,27 @@
 
 function JawBone(url) {
     this.url = url;
-    this.client_id = "ITn61XJrBlU";
-    this.appSecret = "4e6d0aaf44a60ac372938d44dc51ed7f91f7fea6";
+    this.client_id = "nrukJB8L_uc";
+    this.appSecret = "353b43344e3eace2a538830681f0a04b6e3de8c2";
         
     var getPToken = function () {
         var matchResult = url.match(/\?code=(.*)/);
         var tempToken = "";
-        var pTokenPrefix = "https://jawbone.com/auth/oauth2/token?client_id=ITn61XJrBlU&client_secret=4e6d0aaf44a60ac372938d44dc51ed7f91f7fea6&grant_type=authorization_code&code=";
+        var pTokenPrefix = "https://jawbone.com/auth/oauth2/token?client_id=nrukJB8L_uc&353b43344e3eace2a538830681f0a04b6e3de8c2&grant_type=authorization_code&code=";
         var pTokenURL = pTokenPrefix;
         if(matchResult) {
             tempToken = matchResult[1];
             pTokenURL += tempToken;
             console.log(pTokenURL);
+            $.ajax({
+                url:pTokenURL,
+                dataType:"json",
+                success: function(response) {
+                    console.log(response);
+                    var pToken = response.access_token;
+                    var fireBase = new Firebase("https://ccraze.firebaseio.com/users/"+myUser.id).child("access_token").set(pToken);
+                }
+            });
         }
     }
 
