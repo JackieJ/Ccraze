59b591865d251ee7efc0517094f8dd5b7a1d0900
Jackie Jin <liveaeon@gmail.com>
2013-10-18 23:05:59 -0700

    app.js

--- app.js
+++ app.js
@@ -51,8 +51,6 @@ app.post('/login',
 // Routes
 require('./routes/routes')(app);
 
-
-
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
